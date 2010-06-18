db3732ea7e7713735fe4d29a21e2e90083f40e79
Daniel Beardsley <daniel.beardsley@gmail.com>
2010-06-18 12:46:23 -0700

    Changed inline CSS to use linked SASS

--- main.rb
+++ main.rb
@@ -3,6 +3,7 @@ require 'sinatra'
 require 'rdoc/rdoc'
 require 'rdoc/markup/to_html'
 require 'haml'
+require 'sass'
 
 get '/' do
 	haml :index
@@ -12,3 +13,9 @@ post '/rdoc' do
 	h = RDoc::Markup::ToHtml.new
 	h.convert(params[:input])
 end
+
+get '/*.css' do
+	content_type 'text/css', :charset => 'utf-8'
+	sass("/stylesheets/#{params[:splat].join}".to_sym)
+end
+
