8e4827f93739074dfc130d59df7d337a17d0043d
Daniel Beardsley <daniel.beardsley@gmail.com>
2010-06-21 02:30:49 -0700

    Complete rewrite of the app javascript Now loads format type from URL hash

--- public/main.js
+++ public/main.js
@@ -1,15 +1,52 @@
 $(document).ready(function(){
-	var convert_input = function(url){
-		return function(){
-			alert('doin it.');
-			$.post(
-				url,
-				{input:$('#input').val()},
-				function(data) {$('#results').html(data)}
-			);
-		}
+	var state = {format: 'rdoc'}
+
+	var formats = {
+		'rdoc': {button: 'rdoc_btn', response_format: 'html'},
+		'sass': {button: 'sass_btn', response_format: 'css'}
 	}
 
-	$('#rdoc_btn').click(convert_input('/rdoc'));
-	$('#sass_btn').click(convert_input('/sass'));
+
+	load_settings_from_hash();
+
+
+
+// ==  Event Handlers  ==
+
+	$('#go_btn').click(function(){
+		$.post(
+			'/' + state.format,
+			{input:$('#input').val()},
+			function(data) {
+				$('#results').html(data)
+			}
+		);
+	});
+
+
+	$('#format_buttons .button').click(function(event){
+		var format;
+		for(format in formats)
+			if(formats[format] && formats[format].button == event.target.id) break;
+
+		select_format(format);
+		window.location.hash = format;
+		return false;
+	});
+
+
+// ==  Utilities ==
+
+	function load_settings_from_hash(){
+		if(window.location.hash && window.location.hash.length > 0){
+			state.format = window.location.hash.substr(1, 40);
+		}
+		select_format(state.format)
+	}
+
+	function select_format(format){
+		state.format = format;
+		$('#format_buttons .button').removeClass('selected');
+		$('#format_buttons #' + format + '_btn').addClass('selected');
+	}
 })
