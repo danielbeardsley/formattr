b77a601ea29ae91750ac966d702f983171fb695e
Daniel Beardsley <daniel.beardsley@gmail.com>
2010-06-22 16:44:08 -0700

    Properly load and store the output view button state in the hash and make the button state toggled just like the format button

--- public/main.js
+++ public/main.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-	var state = {format: 'rdoc'}
+	var state = {format: 'rdoc', results: 'visual'}
 
 	var formats = {
 		'rdoc': {button: 'rdoc_btn', response_format: 'html'},
@@ -31,16 +31,14 @@ $(document).ready(function(){
 			if(formats[format] && formats[format].button == event.target.id) break;
 
 		select_format(format);
-		window.location.hash = format;
+		save_settings_to_hash();
+
 		return false;
 	});
 
-	$('#visual_output_btn').click(function(event){
-		show_output_panel('visual_output');
-	});
-
-	$('#code_output_btn').click(function(event){
-		show_output_panel('code_output');
+	$('#output_view_buttons .button').click(function(event){
+		show_output_panel(event.target.id.split('_')[0]);
+		save_settings_to_hash();
 	});
 
 
@@ -48,18 +46,40 @@ $(document).ready(function(){
 
 	function load_settings_from_hash(){
 		if(window.location.hash && window.location.hash.length > 0){
-			state.format = window.location.hash.substr(1, 40);
+			var pairs = window.location.hash.substr(1).split('&');
+			for(var i=0,l=pairs.length; i<l; i++){
+				var key_value = pairs[i].split('=');
+				state[key_value[0]] = key_value[1];
+			}
 		}
 		select_format(state.format)
+		show_output_panel(state.results);
 	}
+
+	function save_settings_to_hash(){
+		var pairs = [];
+		for(var k in state){
+			pairs.push([k, state[k]].join('='));
+		}
+		window.location.hash = pairs.join('&');
+	}
+
 
 	function select_format(format){
 		state.format = format;
-		$('#format_buttons .button').removeClass('selected');
-		$('#format_buttons #' + format + '_btn').addClass('selected');
+		select_button(format + '_btn')
 	}
 
+	function select_button(button){
+		$('#' + button).siblings().removeClass('selected');
+		$('#' + button).addClass('selected');
+	}
+
+
 	function show_output_panel(panel_id, immediate){
+		state.results = panel_id;
+		panel_id += '_output';
+		select_button(panel_id + '_btn');
 		$('.result_panel').each(function(i, panel){
 			var action = panel_id == panel.id ? 'show' : 'hide';
 			if(!immediate) action = {show:'fadeIn', hide: 'fadeOut'}[action];
